9961b9fff1c1c33d1c8f9e4c756c53913ed33549
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-05-07 22:38:51 +0200

    testing API

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,32 @@
-#include "Device.h"
-#include "Topology.h"
+#include "API.h"
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
 int main() {
-  Topology *top = new Topology("top1");
-  Device *d1 = new Device("res1", "resistor"), *d2 = new Device("m1", "nmos");
-  top->components["res1"] = d1;
-  top->components["m1"] = d2;
-  d1->netlist["vdd"] = "t1";
-  d1->netlist["n1"] = "t2";
-  vector<pair<string, string>> vec;
-  vec.emplace_back("default", "100");
-  vec.emplace_back("min", "10");
-  vec.emplace_back("max", "1000");
-  d1->properties.emplace_back("resistance", vec);
-  d2->netlist["n1"] = "drain";
-  d2->netlist["vin"] = "gate";
-  d2->netlist["vss"] = "source";
-  vec.clear();
-  vec.emplace_back("default", "1.5");
-  vec.emplace_back("min", "1");
-  vec.emplace_back("max", "2");
-  d2->properties.emplace_back("m(1)", vec);
-  top->print(cout);
-  delete top;
+  TopologyAPI api;
+  cout << boolalpha << api.readJSON("topology.json") << endl;
+  cout << api.readJSON("toptmp.json") << endl;
+  auto vec = api.queryTopologies();
+  for (auto &it : vec) {
+    it->print(cout);
+    cout << endl;
+  }
+  cout << api.deleteTopology("top1") << endl;
+  for (auto &it : vec) {
+    it->print(cout);
+    cout << endl;
+  }
+  auto vecd = api.queryDevices("top2");
+  for (auto &it : vecd) {
+    it->print(cout);
+    cout << endl;
+  }
+  auto vecn = api.queryDevicesWithNetlistNode("top2", "vdd");
+  for (auto &it : vecn) {
+    it->print(cout);
+    cout << endl;
+  }
   return 0;
 }
