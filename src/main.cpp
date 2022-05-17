4a74afe17f40d85f0f13bf387e818b150d7f64c0
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-05-17 19:53:40 +0200

    a better demo for the library

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "API.h"
+#include <API.h>
 #include <iostream>
 #include <memory>
 
@@ -7,26 +7,41 @@ using topology::TopologyAPI;
 
 int main() {
   TopologyAPI api;
-  cout << boolalpha << api.readJSON("topology.json") << endl;
-  cout << api.readJSON("toptmp.json") << endl;
+  cout << "\"topology.json\" file read status = " << boolalpha
+       << api.readJSON("topology.json") << endl;
+  cout << "\"toptmp.json\" file read status = " << api.readJSON("toptmp.json")
+       << endl;
+
+  cout << "All topologies stored in memory now are:" << endl;
   auto vec = api.queryTopologies();
   for (auto &it : vec) {
     it->print(cout);
     cout << endl;
   }
-  cout << api.deleteTopology("top1") << endl;
+
+  cout << "Topology \"top1\" remove from memory state = "
+       << api.deleteTopology("top1") << endl;
+  cout << "Either states would not change the first vector returned from the "
+          "first queryTopologies():"
+       << endl;
   for (auto &it : vec) {
     it->print(cout);
     cout << endl;
   }
   cout << endl;
+
+  cout << "These are all devices stored in topology \"top2\":" << endl;
   auto vecd = api.queryDevices("top2");
   for (auto &it : vecd) {
     it->print(cout);
     cout << endl;
   }
   cout << endl;
-  auto vecn = api.queryDevicesWithNetlistNode("top2", "n1");
+
+  cout << "However, only those devices in topology \"top2\" have a netlist "
+          "\"vss\":"
+       << endl;
+  auto vecn = api.queryDevicesWithNetlistNode("top2", "vss");
   for (auto &it : vecn) {
     it->print(cout);
     cout << endl;
